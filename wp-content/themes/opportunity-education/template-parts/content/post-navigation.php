a26ba9b08ead9c702b93646f4e8f55800d12ba92
Evan Reisberg <evan.reisberg@15four.com>
2018-10-23 11:46:26 -0400

    Refining Blog Navigation and Category Filter

--- wp-content/themes/opportunity-education/template-parts/content/post-navigation.php
+++ wp-content/themes/opportunity-education/template-parts/content/post-navigation.php
@@ -14,17 +14,38 @@ $previous_post = get_previous_post();
 ?>
 
 
-<div class="o-button-container s-no-print u-flex--justify-center">
-
-<?php if ( $previous_post ): ?>
-	<a class="o-button" href="<?php echo get_the_permalink( $previous_post ); ?>">Previous Post</a>
-<?php endif; ?>
-
-	<a class="o-button" href="<?php echo get_the_permalink( get_option( 'page_for_posts' ) ); ?>">Back to News</a>
-
-<?php if ( $next_post ): ?>
-	<a class="o-button" href="<?php echo get_the_permalink( $next_post ); ?>">Next Post</a>
-<?php endif; ?>
-
+<div class="c-grid c-grid--single-row@lg u-flex--justify-between">
+    <div class="c-grid__column c-grid__column--6">
+        <div class="page-links__title">Previous:</div>
+        <?php if ( $previous_post ): ?>
+            <a class="page-links--styled u-text--family-meta-serif u-text--heading-micro" href="<?php echo get_the_permalink( $previous_post ); ?>"><?php echo $previous_post->post_title; ?></a>
+        <?php endif; ?>
+    </div>
+    <div class="c-grid__column c-grid__column--6">
+        <div class="page-links__title">Next:</div>
+        <?php if ( $next_post ): ?>
+            <a class="page-links--styled u-text--family-meta-serif u-text--heading-micro" href="<?php echo get_the_permalink( $next_post ); ?>"><?php echo $next_post->post_title; ?></a>
+        <?php endif; ?>
+    </div>
 </div>
+
+
+
+
+
+
+
+<!--<div class="o-button-container s-no-print u-flex--justify-center">-->
+
+<?php //if ( $previous_post ): ?>
+	<!--<a class="o-button" href="<?php //echo get_the_permalink( $previous_post ); ?>"><?php// echo $previous_post->post_title; ?></a>-->
+<?php //endif; ?>
+
+	<!--<a class="o-button" href="<?php //echo get_the_permalink( get_option( 'page_for_posts' ) ); ?>">Back to News</a>-->
+
+<?php //if ( $next_post ): ?>
+	<!--<a class="o-button" href="<?php //echo get_the_permalink( $next_post ); ?>"><?php //echo $next_post->post_title; ?></a>-->
+<?php //endif; ?>
+
+<!--</div>-->
 
